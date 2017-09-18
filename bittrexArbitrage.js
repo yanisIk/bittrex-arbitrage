71e90f8ad72ae4453913c1cad64e2c9f624e58ab
Yanis Ikene <i.yanis@live.ca>
2017-09-18 01:43:56 -0400

    support for multiple coins at the same time

--- bittrexArbitrage.js
+++ bittrexArbitrage.js
@@ -4,7 +4,7 @@
 //Calculate
 //Execute
 
-const COINS_TO_TRACK = ["OMG"];
+const COINS_TO_TRACK = ["OMG", "BAT", "NEO", "QTUM", "PAY"];
 const EVENTS = {MARKET_UPDATE: "MARKET_UPDATE"}
 
 var bittrex = require('node-bittrex-api');
@@ -23,67 +23,103 @@ function subscribeToPairs() {
     COINS_TO_TRACK.forEach(c => {
         PAIRS_TO_TRACK.push("BTC-"+c);
         PAIRS_TO_TRACK.push("ETH-"+c);
+        //PAIRS_TO_TRACK.push("USDT-"+c);
     });
 
     var pairsData = {};
+    COINS_TO_TRACK.forEach(c => pairsData[c] = {});
+
     pairsDataEventEmitter = new EventEmitter();
     bittrex.websockets.subscribe(PAIRS_TO_TRACK, function(data, client) {      
         if (data.M === 'updateExchangeState') {
-            
+
             data.A.forEach(function(data_for) {
-                if (data_for.Buys.length && data_for.Sells.length)
-                    pairsData[data_for.MarketName] = data_for;
+                if (data_for.Buys.length && data_for.Sells.length) {
+                    var coins = data_for.MarketName.split("-");
+                    var baseCoin = coins[0];
+                    var coin = coins[1];
+                    //If BTC-ETH 
+                    if (baseCoin === "BTC" && coin === "ETH") {
+                        //PUSH IT TO ALL
+                        Object.keys(pairsData).forEach(coin => {
+                            pairsData[coin][data_for.MarketName] = data_for;
+                        });
+                    } else {
+                        pairsData[coin][data_for.MarketName] = data_for;
+                    }
+                }
             });
 
-            if (Object.keys(pairsData).length == PAIRS_TO_TRACK.length) {
-                pairsDataEventEmitter.emit(EVENTS.MARKET_UPDATE, pairsData);
-                pairsData = {};
-            }
+            Object.keys(pairsData).forEach(coin => {
+                if (Object.keys(pairsData[coin]).length === 3 ) {
+                    pairsDataEventEmitter.emit(EVENTS.MARKET_UPDATE, coin, pairsData[coin]);
+                    pairsData[coin] = {};
+                }
+            });
         }
     });
     return pairsDataEventEmitter;
 }
 
 
-function detectArbitrageOpportunity(pairs) {
-    var btcAsk = pairs["BTC-OMG"].Buys[0];
-    var btcSell = pairs["BTC-OMG"].Sells[0];
+/**
+ * TODO: calculate with fees (0.25% / trade , 0.75% total ?)
+ * @param {*} coin 
+ * @param {*} pairs 
+ */
+var plotData = {};
+COINS_TO_TRACK.forEach(c => plotData[c] = []);
+function detectArbitrageOpportunity(coin, pairs) {
+    var btcBid = pairs["BTC-"+coin].Buys[0];
+    var btcAsk = pairs["BTC-"+coin].Sells[0];
 
-    var ethAsk = pairs["ETH-OMG"].Buys[0];
-    var ethSell = pairs["ETH-OMG"].Sells[0];
+    var ethBid = pairs["ETH-"+coin].Buys[0];
+    var ethAsk = pairs["ETH-"+coin].Sells[0];
 
-    var btcEthAsk = pairs["BTC-ETH"].Buys[0];
-    var btcEthSell = pairs["BTC-ETH"].Sells[0];
+    var btcEthBid = pairs["BTC-ETH"].Buys[0];
+    var btcEthAsk = pairs["BTC-ETH"].Sells[0];
 
-    if ((ethSell.Rate*btcEthAsk.Rate) > btcAsk.Rate) {
-        var potentialPercentageWin = ( ( (ethSell.Rate*btcEthAsk.Rate) - btcAsk.Rate ) * btcEthAsk.Rate ) * 100;
-        console.log("BUY IN BTC SELL IN ETH WITH A POTENTIAL OF +"+potentialPercentageWin+" %")
+    if ((ethAsk.Rate*btcEthBid.Rate) > btcBid.Rate) {
+        var potentialPercentageWin = ( ( (ethAsk.Rate*btcEthBid.Rate) - btcBid.Rate ) * btcEthBid.Rate ) * 100;
+        plotData[coin].push(potentialPercentageWin);
+        //console.log("BUY IN BTC SELL IN ETH WITH A POTENTIAL OF +"+potentialPercentageWin+" %")
     }
-    if ((ethSell.Rate*btcEthAsk.Rate) < btcAsk.Rate) {
-        var potentialPercentageWin = ( ( btcAsk.Rate - (ethSell.Rate*btcEthAsk.Rate) ) * btcEthAsk.Rate ) * 100;
-        console.log("BUY IN ETH SELL IN BTC WITH A POTENTIAL OF +"+potentialPercentageWin+" %")
+    if ((ethAsk.Rate*btcEthBid.Rate) < btcBid.Rate) {
+        var potentialPercentageWin = ( ( btcBid.Rate - (ethAsk.Rate*btcEthBid.Rate) ) * btcEthBid.Rate ) * 100;
+        plotData[coin].push(potentialPercentageWin);
+        //console.log("BUY IN ETH SELL IN BTC WITH A POTENTIAL OF +"+potentialPercentageWin+" %")
     }
 
-    if ((btcSell.Rate*btcEthSell.Rate) < ethAsk.Rate) {
-        var potentialPercentageWin = ( ( btcAsk.Rate - (ethSell.Rate*btcEthAsk.Rate) ) * btcEthAsk.Rate ) * 100;
-        console.log("BUY IN ETH SELL IN BTC WITH A POTENTIAL OF +"+potentialPercentageWin+" %")
-    }
-    if ((btcSell.Rate*btcEthSell.Rate) < ethAsk.Rate) {
-        var potentialPercentageWin = ( ( btcAsk.Rate - (ethSell.Rate*btcEthAsk.Rate) ) * btcEthAsk.Rate ) * 100;
-        console.log("BUY IN ETH SELL IN BTC WITH A POTENTIAL OF +"+potentialPercentageWin+" %")
-    }
+    // if ((btcAsk.Rate*btcEthAsk.Rate) < ethBid.Rate) {
+    //     var potentialPercentageWin = ( ( btcBid.Rate - (ethAsk.Rate*btcEthBid.Rate) ) * btcEthBid.Rate ) * 100;
+    //     console.log("BUY IN ETH SELL IN BTC WITH A POTENTIAL OF +"+potentialPercentageWin+" %")
+    // }
+    // if ((btcAsk.Rate*btcEthAsk.Rate) < ethBid.Rate) {
+    //     var potentialPercentageWin = ( ( btcBid.Rate - (ethAsk.Rate*btcEthBid.Rate) ) * btcEthBid.Rate ) * 100;
+    //     console.log("BUY IN ETH SELL IN BTC WITH A POTENTIAL OF +"+potentialPercentageWin+" %")
+    // }
     
 }
 
 
 var marketUpdatesCount = 0;
-subscribeToPairs().on(EVENTS.MARKET_UPDATE, (pairs) => {
+subscribeToPairs().on(EVENTS.MARKET_UPDATE, (coin, pairs) => {
     marketUpdatesCount++;
-    console.log("----- MARKET UPDATE --------" + marketUpdatesCount);
-    detectArbitrageOpportunity(pairs);
-//   Object.keys(pairs).forEach(pair => {
-//       console.log(pair);
-//   }); 
+    //console.log(`----- MARKET UPDATE FOR ${coin} (${Object.keys(pairs).length} pairs) -------- (${marketUpdatesCount})`);
+    detectArbitrageOpportunity(coin, pairs);
+
+    
+    // Object.keys(pairs).forEach(pair => {
+    //     console.log(pair);
+    // }); 
+
+    
 });
+
+setInterval(() => {
+        Object.keys(plotData).forEach(coin => {
+            console.log(`${coin} opportunity: ${plotData[coin]}`);
+        })
+    }, 5000)
 
 
